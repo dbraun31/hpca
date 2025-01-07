85f39dcd17b9f8b53ddbf5f0fa333612139dd062
Dave <dbraun31@gmail.com>
2025-01-07 14:34:27 -0500

    Update example run

--- example_run.r
+++ example_run.r
@@ -6,16 +6,17 @@ source('hpca.r')
 items <- c('att', 'past', 'fut', 'self', 'ppl', 'arou', 'mvmt', 'ling', 'aff')
     
 # Define file path or import the data directly
-file <- 'mweeg_es.csv'
+f <- 'mweeg_es.csv'
+d <- read.csv('mweeg_es.csv')
 
 # Run the analysis
 # Note, groups are not currently in order of similarity
-hpca <- HPCA(file=file, items=items)
+hpca <- HPCA(file='mweeg_es.csv', items=items)
 
 # Determine ideal number of groups
 groups <- get_ideal_groups(hpca)
 
 # Visualize dendrogram and wordcloud (by group)
-plot_dendro(hpca, groups)
+plot_dendro(hpca, 4)
 # if you get the error 'invalid graphics state', try running: dev.off()
-plot_wordcloud(hpca, groups)
+plot_wordcloud(hpca, 4)
